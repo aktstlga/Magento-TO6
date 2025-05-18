0026b4f8418218ce780e3aa75ad2adc6738e2b35
aktstlga <akts.tlga@gmail.com>
2025-05-18 13:29:26 +0300

    Updated

--- src/test/java/runners/RunnerAll.java
+++ src/test/java/runners/RunnerAll.java
@@ -4,7 +4,7 @@ import io.cucumber.testng.AbstractTestNGCucumberTests;
 import io.cucumber.testng.CucumberOptions;
 
 @CucumberOptions(
-        features = {"src/test/java/featureFiles/_03_CreateAddress.feature"},
+        features = {"src/test/java/featureFiles"},
         glue = {"stepDefinitions"}
 )
 public class RunnerAll extends AbstractTestNGCucumberTests {
